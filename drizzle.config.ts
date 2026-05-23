e3831422efc46efae7948448a03f1a658dc69dac
tetsuyaid72 <sekretariatdesatangkas@gmail.com>
2026-05-23 21:01:47 +0800

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: (process.env.TURSO_DATABASE_URL ?? process.env.DATABASE_URL)!,
+    authToken: (process.env.TURSO_AUTH_TOKEN ?? process.env.DATABASE_AUTH_TOKEN)!,
   },
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,15 +2,20 @@ import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
+const databaseUrl = process.env.TURSO_DATABASE_URL ?? process.env.DATABASE_URL;
+const databaseAuthToken =
+  process.env.TURSO_AUTH_TOKEN ?? process.env.DATABASE_AUTH_TOKEN;
+
 const client = createClient(
-  process.env.TURSO_DATABASE_URL
+  databaseUrl
     ? {
-        url: process.env.TURSO_DATABASE_URL,
-        authToken: process.env.TURSO_AUTH_TOKEN!,
+        url: databaseUrl,
+        authToken: databaseAuthToken,
       }
     : {
         url: "file:sqlite.db",
       }
 );
+
 
 export const db = drizzle(client, { schema });
